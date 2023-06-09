d3b1509fb5bed72fe40f751fb0f8107abecdcf7a
pawn <devrunner21@gmail.com>
2023-06-09 16:58:32 +0900

    chore: 배포 테스트 커밋

--- src/main/java/com/nexters/pinataserver/common/validation/ValidEnum.java
+++ src/main/java/com/nexters/pinataserver/common/validation/ValidEnum.java
@@ -13,7 +13,11 @@ import javax.validation.Payload;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ValidEnum {
 	String message() default "Invalid value. This is not permitted.";
+
 	Class<?>[] groups() default {};
+
 	Class<? extends Payload>[] payload() default {};
+
 	Class<? extends java.lang.Enum<?>> enumClass();
+
 }
